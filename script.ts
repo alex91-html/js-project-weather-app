f0f50e10f5630458b2bf64184c1a01035513e647
alex91-html <alex91-html@users.noreply.github.com>
2025-03-19 09:09:23 +0100

    API url

--- script.ts
+++ script.ts
@@ -1,11 +1,14 @@
 //oscar was here12131
 // hello :D
-
 document.addEventListener("DOMContentLoaded", () => {
 
 
 
-  const URL = ``;
+
+
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=Stockholm&units=metric&APPID=f70fe821b1e9718ced63c3a6bf1070e4`;
+
+
 
 
 
